519aeda7cadb31bfa6b52f5e7c54cbdd3c1ac97e
jeydi243 <kadiongoe@yahoo.fr>
2020-04-22 11:55:15 +0000

    fix genre field

--- src/components/Etudiants/inscription.vue
+++ src/components/Etudiants/inscription.vue
@@ -1,239 +1,246 @@
 <template>
-    <v-container>
-        <h1>Formulaire d'Inscription</h1>
-        <v-stepper v-model="e6" vertical>
-            <v-stepper-step :complete="e6 > 1" step="1" editable>Informations de Base</v-stepper-step>
-            <v-stepper-content step="1">
-                <v-container fluid>
-                    <v-row>
-                        <v-col md="8">
-                            <v-card raised shaped>
-                                <v-container>
-                                    <v-row>
-                                        <v-col cols="6">
-                                            <v-text-field v-model="firstname" :rules="nameRules" :counter="10"
-                                                label="Nom" required filled>
-                                            </v-text-field>
-                                        </v-col>
-                                        <v-col cols="6">
-                                            <v-text-field v-model="lastname" :rules="nameRules" :counter="10"
-                                                label="Post-nom" required filled> </v-text-field>
-                                        </v-col>
-                                    </v-row>
-                                    <v-row>
-                                        <v-col cols="6">
-                                            <v-text-field v-model="nickname" :rules="nameRules" label="Prenom" required filled>
-                                            </v-text-field>
-                                        </v-col>
-                                        <v-col cols="6">
-                                            <v-text-field v-model="telephone" :rules="telRules"
-                                                label="Numero de telephone" required filled>
-                                            </v-text-field>
-                                        </v-col>
-                                    </v-row>
-                                    <v-row>
-                                        <v-col md="4">
-                                            <header>Genre</header>
-                                            <v-radio-group v-model="genre" :mandatory="false" row>
-                                                <v-radio label="F" value="F"></v-radio>
-                                                <v-radio label="M" value="M"></v-radio>
-                                            </v-radio-group>
-                                        </v-col>
-                                        <v-spacer></v-spacer>
-                                        <v-col md="6">
-                                            <v-menu v-model="menu" :close-on-content-click="false"
-                                                transition="scale-transition" ref="menu" offset-y>
-                                                <template v-slot:activator="{ on }">
-                                                    <v-text-field v-model="date" label="Date de naissance" readonly
-                                                        v-on="on">
-                                                    </v-text-field>
-                                                </template>
-                                                <v-date-picker ref="picker" v-model="date"
-                                                    :max="new Date().toISOString().substr(0, 10)" min="1996-01-01"
-                                                    @change="save">
-                                                </v-date-picker>
-                                            </v-menu>
-                                        </v-col>
-                                    </v-row>
-                                    <v-row>
-                                        <v-col md="6">
-                                            <v-text-field v-model="adresse" label="Adresse physique"></v-text-field>
-                                        </v-col>
-                                        <v-col md="6">
-                                            <v-text-field v-model="emailPerso" label="Email personnel">
-                                            </v-text-field>
-                                        </v-col>
+	<v-container>
+		<h1>Formulaire d'Inscription</h1>
+		<v-stepper v-model="e6" vertical>
+			<v-stepper-step :complete="e6 > 1" step="1" editable>Informations de Base</v-stepper-step>
+			<v-stepper-content step="1">
+				<v-container fluid>
+					<v-row>
+						<v-col md="8">
+							<v-card raised shaped>
+								<v-container>
+									<v-row>
+										<v-col cols="6">
+											<v-text-field
+												v-model="firstname"
+												:rules="nameRules"
+												:counter="10"
+												label="Nom"
+												required
+												filled
+											></v-text-field>
+										</v-col>
+										<v-col cols="6">
+											<v-text-field
+												v-model="lastname"
+												:rules="nameRules"
+												:counter="10"
+												label="Post-nom"
+												required
+												filled
+											></v-text-field>
+										</v-col>
+									</v-row>
+									<v-row>
+										<v-col cols="6">
+											<v-text-field v-model="nickname" :rules="nameRules" label="Prenom" required filled></v-text-field>
+										</v-col>
+										<v-col cols="6">
+											<v-text-field
+												v-model="telephone"
+												:rules="telRules"
+												label="Numero de telephone"
+												required
+												filled
+											></v-text-field>
+										</v-col>
+									</v-row>
+									<v-row>
+										<v-col md="6">
+											<v-select v-model="genre" label="Genre" :items="genres" required filled></v-select>
+										</v-col>
+										<v-spacer></v-spacer>
+										<v-col md="6">
+											<v-menu
+												v-model="menu"
+												:close-on-content-click="false"
+												transition="scale-transition"
+												ref="menu"
+												offset-y
+											>
+												<template v-slot:activator="{ on }">
+													<v-text-field v-model="date" label="Date de naissance" readonly v-on="on" filled></v-text-field>
+												</template>
+												<v-date-picker
+													ref="picker"
+													v-model="date"
+													:max="new Date().toISOString().substr(0, 10)"
+													min="1996-01-01"
+													@change="save"
+												></v-date-picker>
+											</v-menu>
+										</v-col>
+									</v-row>
+									<v-row>
+										<v-col md="6">
+											<v-text-field v-model="adresse" label="Adresse physique" filled></v-text-field>
+										</v-col>
+										<v-col md="6">
+											<v-text-field v-model="emailPerso" label="Email personnel" filled></v-text-field>
+										</v-col>
+									</v-row>
+									<v-row>
+										<v-col md="12">
+											<v-textarea v-model="noteSante" label="Note de sante personnel" solo name="input-7-4"></v-textarea>
+										</v-col>
+									</v-row>
+								</v-container>
+							</v-card>
+						</v-col>
+						<v-col md="4">
+							<profile />
+						</v-col>
+					</v-row>
+				</v-container>
+				<v-btn color="primary" @click="e6 = 2">Continue</v-btn>
+			</v-stepper-content>
 
-                                    </v-row>
-                                    <v-row>
-                                        <v-col md="12">
-                                            <v-textarea v-model="noteSante" label="Note de sante personnel" solo
-                                                name="input-7-4"> </v-textarea>
-                                        </v-col>
-                                    </v-row>
-                                </v-container>
-                            </v-card>
-                        </v-col>
-                        <v-col md="4">
-                            <profile />
-                        </v-col>
-                    </v-row>
-                </v-container>
-                <v-btn color="primary" @click="e6 = 2">Continue</v-btn>
-            </v-stepper-content>
+			<v-stepper-step :complete="e6 > 2" step="2" editable>Informations Complementaires</v-stepper-step>
+			<v-stepper-content step="2">
+				<v-container fluid>
+					<v-row>
+						<v-col md="3">
+							<v-text-field v-model="ecoleOrigine" readonly label="Nom ecole origine" required></v-text-field>
+						</v-col>
+						<v-col md="3">
+							<v-text-field v-model="adresseEcole" label="Adresse ecole" required></v-text-field>
+						</v-col>
+						<v-col md="3">
+							<v-text-field v-model="sectionObtention" label="Section obtention diplome" required></v-text-field>
+						</v-col>
+						<v-col md="2">
+							<v-text-field v-model="pourcentageExetat" label="Pourcentage exetat" required></v-text-field>
+						</v-col>
+					</v-row>
+					<v-row>
+						<v-col md="2">
+							<v-text-field v-model="anneeInscription" label="Annee Inscription" disabled required></v-text-field>
+						</v-col>
+						<v-col md="2">
+							<v-text-field v-model="pourcentageObtenuTest" label="% test admission" required></v-text-field>
+						</v-col>
+						<v-col md="2">
+							<v-select :items="niveau" label="Niveau academique"></v-select>
+						</v-col>
+						<v-col md="2">
+							<v-select :items="statuts" label="Statut academique" required></v-select>
+						</v-col>
+						<v-col md="2">
+							<v-text-field
+								v-model="dateDiplomeEsis"
+								readonly
+								label="Date Diplome Esis"
+								hint="a definir prochainement"
+								required
+							></v-text-field>
+						</v-col>
+					</v-row>
+					<v-btn color="primary" @click="e6 = 3">Continue</v-btn>
+				</v-container>
+			</v-stepper-content>
 
-            <v-stepper-step :complete="e6 > 2" step="2" editable>Informations Complementaires</v-stepper-step>
-            <v-stepper-content step="2">
-                <v-container fluid>
-                    <v-row>
-                        <v-col md="3">
-                            <v-text-field v-model="ecoleOrigine" readonly label="Nom ecole origine" required>
-                            </v-text-field>
-                        </v-col>
-                        <v-col md="3">
-                            <v-text-field v-model="adresseEcole" label="Adresse ecole" required>
-                            </v-text-field>
-                        </v-col>
-                        <v-col md="3">
-                            <v-text-field v-model="sectionObtention" label="Section obtention diplome" required>
-                            </v-text-field>
-                        </v-col>
-                        <v-col md="2">
-                            <v-text-field v-model="pourcentageExetat" label="Pourcentage exetat" required>
-                            </v-text-field>
-                        </v-col>
+			<v-stepper-step :complete="e6 > 3" step="3" editable>Informations du Responsables</v-stepper-step>
+			<v-stepper-content step="3">
+				<v-row>
+					<v-col md="3">
+						<v-text-field v-model="responsableNom" label="Nom responsable"></v-text-field>
+					</v-col>
+					<v-col md="3">
+						<v-text-field v-model="responsableNumero" label="Telephone responsable"></v-text-field>
+					</v-col>
+					<v-col md="3">
+						<v-text-field v-model="responsableEmail" label="Email responsable"></v-text-field>
+					</v-col>
+					<v-col md="3">
+						<v-btn @click="addResponsable" rounded color="teal">Ajouter</v-btn>
+					</v-col>
+				</v-row>
+				<v-btn color="primary" @click="e6 = 4">Continue</v-btn>
+			</v-stepper-content>
 
-                    </v-row>
-                    <v-row>
-                        <v-col md="2">
-                            <v-text-field v-model="anneeInscription" label="Annee Inscription" disabled required>
-                            </v-text-field>
-                        </v-col>
-                        <v-col md="2">
-                            <v-text-field v-model="pourcentageObtenuTest" label="% test admission" required>
-                            </v-text-field>
-                        </v-col>
-                        <v-col md="2">
-                            <v-select :items="niveau" label="Niveau academique"></v-select>
-                        </v-col>
-                        <v-col md="2">
-                            <v-select :items="statuts" label="Statut academique" required></v-select>
-                        </v-col>
-                        <v-col md="2">
-                            <v-text-field v-model="dateDiplomeEsis" readonly label="Date Diplome Esis"
-                                hint="a definir prochainement" required></v-text-field>
-                        </v-col>
-                    </v-row>
-                    <v-btn color="primary" @click="e6 = 3">Continue</v-btn>
-                </v-container>
-
-            </v-stepper-content>
-
-            <v-stepper-step :complete="e6 > 3" step="3" editable>Informations du Responsables</v-stepper-step>
-            <v-stepper-content step="3">
-                <v-row>
-                    <v-col md="3">
-                        <v-text-field v-model="responsableNom" label="Nom responsable"></v-text-field>
-                    </v-col>
-                    <v-col md="3">
-                        <v-text-field v-model="responsableNumero" label="Telephone responsable">
-                        </v-text-field>
-                    </v-col>
-                    <v-col md="3">
-                        <v-text-field v-model="responsableEmail" label="Email responsable">
-                        </v-text-field>
-                    </v-col>
-                    <v-col md="3">
-                        <v-btn @click="addResponsable" rounded color="teal">Ajouter</v-btn>
-                    </v-col>
-                </v-row>
-                <v-btn color="primary" @click="e6 = 4">Continue</v-btn>
-
-            </v-stepper-content>
-
-            <v-stepper-step step="4" editable>View setup instructions</v-stepper-step>
-            <v-stepper-content step="4">
-                <v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
-                <v-btn color="primary" @click="e6 = 1">Continue</v-btn>
-                <v-btn text>Cancel</v-btn>
-            </v-stepper-content>
-        </v-stepper>
-    </v-container>
+			<v-stepper-step step="4" editable>View setup instructions</v-stepper-step>
+			<v-stepper-content step="4">
+				<v-card color="grey lighten-1" class="mb-12" height="200px"></v-card>
+				<v-btn color="primary" @click="e6 = 1">Continue</v-btn>
+				<v-btn text>Cancel</v-btn>
+			</v-stepper-content>
+		</v-stepper>
+	</v-container>
 </template>
 <script>
-import profile from '@/components/Etudiants/profile.vue'
-    export default {
-        name: "inscription",
-        components:{
-            profile
-        },
-        data() {
-            return {
-                e6: 1,
-                pourcentageExetat:0,
-                sectionObtention:"",
-                adresseEcole:"",
-                ecoleOrigine:"",
-                genre:"F",
-                noteSante:"",
-                telephone:"",
-                emailPerso:"",
-                dateDiplomeEsis:Date,
-                responsables: [{}],
-                responsableNom: "",
-                responsableNumero: "",
-                responsableEmail: "",
-                niveau: ["PREPA", "G1", "G2", "G3"],
-                again1: false,
-                pourcentageObtenuTest: 0,
-                again2: false,
-                statuts: ["Candidat", "Etudiant", "Diplomé", "Abandon", "Renvoi"],
-                date: null,
-                valid: false,
-                menu: false,
-                genres: ["M", "F"],
-                adresse: "",
-                anneeInscription: new Date().getFullYear().toString(),
-                tel: '',
-                firstname: '',
-                nickname: '',
-                lastname: '',
-                telRules: [v => !!v || 'Ce champ est obligatoire'],
-                nameRules: [
-                    v => !!v || 'Ce champ  est obligatoire',
-                    v => v.length <= 10 || '10 caracteres au plus sont permis',
-                ],
-                email: '',
-                emailRules: [
-                    v => !!v || "l'email est obligatoire",
-                    v => /.+@.+/.test(v) || "l'Email doit etre valide",
-                ],
-            }
-        },
-        watch: {
-            menu(val) {
-                val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'))
-            },
-        },
-        methods: {
-            save(date) {
-                this.$refs.menu.save(date)
-            },
-            affiche() {
-                console.log("la methode afficher a été appelé ");
-            },
-            addResponsable() {
-                this.responsables.add();
-            }
-        },
-    }
+import profile from "@/components/Etudiants/profile.vue";
+export default {
+	name: "inscription",
+	components: {
+		profile
+	},
+	data() {
+		return {
+			e6: 1,
+			pourcentageExetat: 0,
+			sectionObtention: "",
+			adresseEcole: "",
+			ecoleOrigine: "",
+			genre: "F",
+			noteSante: "",
+			telephone: "",
+			emailPerso: "",
+			dateDiplomeEsis: Date,
+			responsables: [{}],
+			responsableNom: "",
+			responsableNumero: "",
+			responsableEmail: "",
+			niveau: ["PREPA", "G1", "G2", "G3"],
+			again1: false,
+			pourcentageObtenuTest: 0,
+			again2: false,
+			statuts: ["Candidat", "Etudiant", "Diplomé", "Abandon", "Renvoi"],
+			date: null,
+			valid: false,
+			menu: false,
+			genres: ["M", "F"],
+			adresse: "",
+			anneeInscription: new Date().getFullYear().toString(),
+			tel: "",
+			firstname: "",
+			nickname: "",
+			lastname: "",
+			telRules: [v => !!v || "Ce champ est obligatoire"],
+			nameRules: [
+				v => !!v || "Ce champ  est obligatoire",
+				v => v.length <= 10 || "10 caracteres au plus sont permis"
+			],
+			email: "",
+			emailRules: [
+				v => !!v || "l'email est obligatoire",
+				v => /.+@.+/.test(v) || "l'Email doit etre valide"
+			]
+		};
+	},
+	watch: {
+		menu(val) {
+			val && setTimeout(() => (this.$refs.picker.activePicker = "YEAR"));
+		}
+	},
+	methods: {
+		save(date) {
+			this.$refs.menu.save(date);
+		},
+		affiche() {
+			console.log("la methode afficher a été appelé ");
+		},
+		addResponsable() {
+			this.responsables.add();
+		}
+	}
+};
 </script>
 
 <style>
-    img {
-        border-radius: 50%;
-    }
-    #btnDownload {
-        margin: -25px;
-    }
+img {
+	border-radius: 50%;
+}
+#btnDownload {
+	margin: -25px;
+}
 </style>
