ada16e65d77960c14b2dcc800b2050b0d2e88bc0
jeydi243 <ilungakadiongo@gmail.com>
2022-07-10 20:42:45 +0200

    App.vue optionsAPI to CompositionAPI

---
+++ src/router/views/management/employees/update.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="row">
+        <div class="col">fee</div>
+    </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue"
+import { mapActions, mapGetters, useStore } from 'vuex'
+
+
+
+</script>
